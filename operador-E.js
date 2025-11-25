deb1f7bd15f56137ce8a9970f5397d583c534ca1
ueberson672 <ueberson.marques@icloud.com>
2025-11-24 21:18:23 -0300

    Add files via upload

---
+++ operador-E.js
@@ -0,0 +1,9 @@
+let maiorIdade = true
+let temIngresso = true
+
+if(maiorIdade && temIngresso) {
+    alert("Entrada permitida")
+}
+else{
+    alert("Entrada negada")
+}
---
+++ operador-OU.js
@@ -0,0 +1,8 @@
+let temIngresso = false
+let nomeNaLista = false
+
+if(temIngresso || nomeNaLista) {
+    alert("Entrada permitida!")
+}else {
+    alert("Entrada negada!")
+}
